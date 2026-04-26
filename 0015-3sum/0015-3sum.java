1377f63605092a20def5becad4d7c5f1a0eabeb4
Utsav Bajpai <utsavb736@gmail.com>
2026-04-27 00:32:18 +0530

    Time: 33 ms (74%), Space: 58.8 MB (90.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,33 +2,22 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // skip duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // skip duplicates
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+        
+        for(int i = 0; i < nums.length-2; i++){
+            int pt1 = i,pt3 = nums.length-1, pt2 = i+1;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            while(pt2 < pt3){
+                int sum = nums[pt1] + nums[pt2] + nums[pt3];
+                if(sum > 0){
+                    pt3--;
+                }else if (sum < 0){
+                    pt2++;
+                }else{
+                    ans.add(Arrays.asList(nums[pt1],nums[pt2],nums[pt3]));
+                    while(pt2 < pt3 && nums[pt2] == nums[pt2+1]) pt2++;
+                    while(pt2 < pt3 && nums[pt3] == nums[pt3-1]) pt3--;
+                    pt2++;
+                    pt3--;
                 }
             }
         }
