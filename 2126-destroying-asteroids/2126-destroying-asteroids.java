f490e8288e63ecb6232095a60cdd40a6b13076ec
Utsav Bajpai <utsavb736@gmail.com>
2026-05-31 23:36:41 +0530

    Time: 26 ms (81.52%), Space: 107.5 MB (30.95%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         int n = asteroids.length;
-
         Arrays.sort(asteroids);
-
         long sum = mass;
-
         for(int i=0;i<n;i++){
             if(asteroids[i] > sum) return false;
-
             sum += asteroids[i];
         }
-
         return true;
     }
 }
