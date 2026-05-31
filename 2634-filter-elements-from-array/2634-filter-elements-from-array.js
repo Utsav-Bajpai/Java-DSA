6edd35dff765de010eb7ba30a541a20fb129fb01
Utsav Bajpai <utsavb736@gmail.com>
2026-06-01 00:08:01 +0530

    Time: 42 ms (66.61%), Space: 53.9 MB (25.75%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,9 +4,5 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let fil = [];
-    for(let i = 0; i < arr.length; i++){
-        if(fn(arr[i], i)) fil.push(arr[i]);
-    }
-    return fil;
+    return arr.filter(fn);
 };
