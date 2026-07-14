978f3d81c02b00bfd520c19b3640ed6d132518a0
Utsav Bajpai <utsavb736@gmail.com>
2026-07-15 00:23:58 +0530

    Time: 5 ms (99.61%), Space: 45.6 MB (20.8%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,31 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(0, target, candidates, new ArrayList<>(), ans);
+        comb(0, candidates, target, ans, new ArrayList<>());
         return ans;
     }
-
-    void backtrack(int idx, int target, int[] arr,
-                   List<Integer> ds, List<List<Integer>> ans) {
-
-        if (target == 0) {
+    void comb(int idx, int[] arr, int tar, List<List<Integer>> ans, List<Integer> ds){
+        if(tar == 0){
             ans.add(new ArrayList<>(ds));
             return;
         }
-
-        for (int i = idx; i < arr.length; i++) {
-
-            if (i > idx && arr[i] == arr[i - 1])
-                continue;
-
-            if (arr[i] > target)
-                break;
-
+        for(int i = idx; i < arr.length; i++){
+            if(i > idx && arr[i] == arr[i-1]) continue;
+            if(arr[i] > tar) break;
             ds.add(arr[i]);
-            backtrack(i + 1, target - arr[i], arr, ds, ans);
-            ds.remove(ds.size() - 1);
+            comb(i + 1, arr, tar - arr[i], ans, ds);
+            ds.remove(ds.size()-1);
         }
     }
 }
