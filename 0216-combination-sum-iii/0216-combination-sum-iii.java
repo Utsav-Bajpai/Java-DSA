31bb1ecdc0fa24df32080e0e8b6337a7ce564585
Utsav Bajpai <utsavb736@gmail.com>
2026-07-17 00:06:47 +0530

    Time: 0 ms (100%), Space: 42.4 MB (49.22%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,22 +4,17 @@ class Solution {
         comb3(1, k, n, ans, new ArrayList<>());
         return ans;
     }
-
-    void comb3(int idx, int k, int tar, List<List<Integer>> ans, List<Integer> ds) {
-
+    void comb3(int idx, int k, int tar, List<List<Integer>> ans, List<Integer> ds){
         if(ds.size() == k){
-            if(tar == 0){
-                ans.add(new ArrayList<>(ds));
-            }
+            if(tar == 0) ans.add(new ArrayList<>(ds));
             return;
         }
-
         for(int i = idx; i <= 9; i++){
             if(i > tar) break;
-
             ds.add(i);
             comb3(i + 1, k, tar - i, ans, ds);
-            ds.remove(ds.size() - 1);
+            ds.remove(ds.size()-1);
+
         }
     }
 }
