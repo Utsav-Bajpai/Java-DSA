870cc205cc20a6f6428c3332d385f7ea8ee8d482
Utsav Bajpai <utsavb736@gmail.com>
2026-07-31 00:32:45 +0530

    Time: 4 ms (73.96%), Space: 44.9 MB (81.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] arr = new int[nums1.length];
-        for(int i = 0; i < nums1.length; i++){
-            Stack<Integer> stk = new Stack<>();
-            for(int j = nums2.length-1; j >= 0; j--){
-                while(!stk.isEmpty() && stk.peek() <= nums2[j]){
-                    stk.pop();
-                }
-                if(stk.isEmpty()) arr[i] = -1;
-                else arr[i] = stk.peek();
-                stk.push(nums2[j]);
-                if(nums1[i] == nums2[j]) break;
+        Map<Integer, Integer> hs = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+        for(int j = nums2.length-1; j >= 0; j--){
+            while(!stk.isEmpty() && stk.peek() <= nums2[j]){
+                stk.pop();
             }
+            if(stk.isEmpty()) hs.put(nums2[j], -1);
+            else hs.put(nums2[j], stk.peek());
+            stk.push(nums2[j]);
+        }
+        int idx = 0;
+        for(int i : nums1){
+            arr[idx] = hs.get(i);
+            idx++;
         }
         return arr;
     }
