f6be245abf70d620b143bda0860d1c60d09ef8e6
Utsav Bajpai <utsavb736@gmail.com>
2026-08-01 02:10:31 +0530

    Time: 15 ms (61.46%), Space: 48.5 MB (60.47%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,21 +3,15 @@ class Solution {
         int n = nums.length;
         int[] arr = new int[n];
         Stack<Integer> stk = new Stack<>();
-
-        for (int i = 2 * n - 1; i >= 0; i--) {
+        for(int i = (2 * n) - 1; i >= 0; i--){
             int idx = i % n;
-
-            while (!stk.isEmpty() && stk.peek() <= nums[idx]) {
+            while(!stk.isEmpty() && stk.peek() <= nums[idx]){
                 stk.pop();
             }
-
-            if (i < n) {
-                arr[idx] = stk.isEmpty() ? -1 : stk.peek();
-            }
-
+            if(i < n) arr[i] = stk.isEmpty() ? -1 : stk.peek();
             stk.push(nums[idx]);
+            
         }
-
         return arr;
     }
 }
