9db7941a9e9411684d2eec1e23ed2cd763758908
Utsav Bajpai <utsavb736@gmail.com>
2026-08-08 23:59:26 +0530

    Time: 61 ms (75.21%), Space: 78.1 MB (49.99%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,22 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] psei = PSEI(heights);
-        int[] nsei = NSEI(heights);
-        int ma = 0;
+        int marea = 0;
+        Stack<Integer> stk = new Stack<>();
         for(int i = 0; i < heights.length; i++){
-            int area = heights[i] * (nsei[i] - psei[i] -1);
-            ma = Math.max(area, ma);
-        }
-        return ma;
-
-    }
-    int[] PSEI(int[] arr){
-        Stack<Integer> stk = new Stack<>();
-        int[] nums = new int[arr.length];
-        for(int i = 0 ; i < arr.length; i++){
-            while(!stk.isEmpty() && arr[stk.peek()] >= arr[i]){
-                stk.pop();
+            while(!stk.isEmpty() && heights[stk.peek()] > heights[i]){
+                int ele = stk.pop();
+                int nse = i;
+                int pse = stk.isEmpty() ? -1 : stk.peek();
+                marea = Math.max(marea, heights[ele]*(nse - pse - 1));
             }
-            nums[i] = stk.isEmpty() ? -1 : stk.peek();
             stk.push(i);
         }
-        return nums;
-    }
-    int[] NSEI(int[] arr){
-        Stack<Integer> stk = new Stack<>();
-        int n = arr.length;
-        int[] nums = new int[n];
-        for(int i = arr.length - 1; i >= 0; i--){
-            while(!stk.isEmpty() && arr[stk.peek()] >= arr[i]){
-                stk.pop();
-            }
-            nums[i] = stk.isEmpty() ? n : stk.peek();
-            stk.push(i);
+        while(!stk.isEmpty()){
+            int ele = stk.pop();
+            int nse = heights.length;
+            int pse = stk.isEmpty() ? -1 : stk.peek();
+            marea = Math.max(marea, heights[ele]*(nse - pse - 1));
         }
-        return nums;
+        return marea;
     }
 }
