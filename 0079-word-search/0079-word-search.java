3b006e24e34eee172beb0fd5bcfb3f1bb3319d7e
Utsav Bajpai <utsavb736@gmail.com>
2026-08-10 11:17:06 +0530

    Time: 137 ms (51.02%), Space: 43.2 MB (28.6%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,18 +2,25 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                if(dfs(board, word, i, j, 0)) return true;
+                if(board[i][j] == word.charAt(0)){
+                    boolean ans = dfs(board, i, j, 0, word);
+                    if(ans) return true;
+                }
             }
         }
         return false;
     }
-    boolean dfs(char[][] board, String word, int i, int j, int idx){
-        if(idx == word.length()) return true;
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx)) return false;
+    boolean dfs(char[][] board, int i, int j, int idx, String word){
+        if(idx > word.length()-1) return true;
+        if(i < 0 || i > board.length - 1 || j < 0 || j > board[0].length -1 ) return false;
+        if(board[i][j] != word.charAt(idx)) return false;
         char temp = board[i][j];
-        board[i][j] = '*';
-        boolean found = dfs(board, word, i+1, j, idx+1) || dfs(board, word, i-1, j, idx+1) || dfs(board, word, i, j+1, idx+1) || dfs(board, word, i, j-1, idx+1);
+        board[i][j] = '#';
+        boolean up = dfs(board, i+1, j, idx + 1, word);
+        boolean down = dfs(board, i-1, j, idx + 1, word);
+        boolean left = dfs(board, i, j+1, idx + 1, word);
+        boolean right = dfs(board, i, j-1, idx + 1, word);
         board[i][j] = temp;
-        return found;
+        return (up || down || left || right);
     }
 }
