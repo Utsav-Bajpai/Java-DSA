b470214f12fee028e74b41b6d1b02865a3a85e16
Utsav Bajpai <utsavb736@gmail.com>
2026-08-23 00:39:59 +0530

    Time: 53 ms (15.53%), Space: 50.2 MB (57.58%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,37 +1,20 @@
 class Solution {
-
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
+        return nSA(nums, k) - nSA(nums, k-1);
     }
-
-    private int atMost(int[] nums, int k) {
-        if (k == 0) return 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int i = 0;
-        int count = 0;
-
-        for (int j = 0; j < nums.length; j++) {
-
-            // Add nums[j]
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
-            // Too many distinct numbers
-            while (map.size() > k) {
-                map.put(nums[i], map.get(nums[i]) - 1);
-
-                if (map.get(nums[i]) == 0) {
-                    map.remove(nums[i]);
-                }
-
+    int nSA(int[] nums, int k){
+        int i = 0, j = 0, count = 0;
+        Map<Integer, Integer> hm = new HashMap<>();
+        while(j < nums.length){
+            hm.put(nums[j], hm.getOrDefault(nums[j], 0) + 1);
+            while(hm.size() > k){
+                hm.put(nums[i], hm.get(nums[i]) - 1 );
+                if(hm.get(nums[i]) == 0) hm.remove(nums[i]);
                 i++;
             }
-
-            // Every subarray ending at j is valid
             count += j - i + 1;
+            j++;
         }
-
         return count;
     }
 }
