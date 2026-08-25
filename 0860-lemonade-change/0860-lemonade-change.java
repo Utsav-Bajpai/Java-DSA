8b0b9f21cd56e47ecb0444d2b6d9b86e68dafb33
Utsav Bajpai <utsavb736@gmail.com>
2026-08-25 23:22:02 +0530

    Time: 2 ms (98.02%), Space: 72.4 MB (68.65%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0; 
+        for(int num : bills){
+            if(num == 5){
+                five++;
+            }else if(num == 10){
+                if(five < 1) return false;
+                ten++;
+                five--;
+            }else{
+                if(five > 0 && ten > 0){
+                    five--;
+                    ten--;
+                }else if(five >= 3) five -= 3;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
