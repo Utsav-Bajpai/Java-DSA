a73a012041ef36a5162f2f4242777353fd592ecd
Utsav Bajpai <utsavb736@gmail.com>
2026-08-31 00:14:34 +0530

    Time: 2 ms (89.04%), Space: 48.1 MB (23.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            if(i > maxReach) {
-                return false;
-            }
-
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            if(maxReach >= nums.length - 1) {
-                return true;
-            }
+        int mIdx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > mIdx) return false;
+            mIdx = Math.max(mIdx, nums[i] + i);
         }
-
         return true;
     }
 }
